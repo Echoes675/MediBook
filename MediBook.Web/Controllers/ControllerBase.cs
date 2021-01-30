951699e5e45a6e27663b55b751292608a88d55af
EMcGonagle <eoin@alphaepsilon.co.uk>
2021-01-30 19:39:03 +0000

    Added remaining required projects to solution

---
+++ MediBook.Web/Controllers/ControllerBase.cs
@@ -0,0 +1,24 @@
+﻿namespace MediBook.Web.Controllers
+{
+    using MediBook.Web.Enums;
+    using Microsoft.AspNetCore.Mvc;
+
+    // Enumeration of the different alert types
+
+    /// <summary>
+    /// ControllerBase provides alert functionality to the derived Controller classes
+    /// </summary>
+    public class ControllerBase : Controller
+    {
+        /// <summary>
+        /// Set the details of the alert into TempData
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="type"></param>
+        public void Alert(string message, AlertType type = AlertType.Info)
+        {
+            TempData["Alert.Message"] = message;
+            TempData["Alert.Type"] = type.ToString();
+        }
+    }
+}
